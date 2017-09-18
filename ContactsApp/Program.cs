bd2af409cdd3df15b707bc7e9371b5233b36f009
nhatanph <31908615+nhatanph@users.noreply.github.com>
2017-09-18 22:41:52 +0700

    Bổ sung xử lý đọc và lưu tập tin
    Bổ sung lớp tiện ích XuLyFile.cs có hai hàm tiện ích LayDuLieuTuFile và LayDuLieuRaFile
    Lớp program cũng được chỉnh lại

--- ContactsApp/Program.cs
+++ ContactsApp/Program.cs
@@ -11,6 +11,12 @@ namespace ContactsApp
         static void Main(string[] args)
         {
             List<Contact> danhba = new List<Contact>();
+            List<Contact> danhba = new List<Contact>();
+
+            // doc du lieu tu file
+            if (File.Exists("contacts.txt"))
+                XuLyFile.LayDuLieuTuFile(danhba);
+
             char r;
             do
             {
@@ -40,8 +46,10 @@ namespace ContactsApp
                                  c.GetDiaChi(),
                                  c.GetSDT());
             }
+
+            XuLyFile.LuuDuLieuRaFile(danhba);
             Console.ReadLine();
         }
     }
-    }
+}
 
